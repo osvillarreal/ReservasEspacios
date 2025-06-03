53666d0d8db9b48b37e1fe40635ce0cc70bd1c94
osvillarreal <villarrealsebastian2006@gmail.com>
2025-06-03 09:26:15 -0500

    Update AdminInsertController.java

--- src/main/java/com/reservas/app/controller/AdminInsertController.java
+++ src/main/java/com/reservas/app/controller/AdminInsertController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class AdminInsertController {
 
     @Autowired
@@ -15,11 +16,10 @@ public class AdminInsertController {
     @GetMapping("/crear-admin")
     public String crearAdmin() {
         Usuario admin = new Usuario();
-        admin.setNombre("Admin Prueba");
-        admin.setCorreo("admin@prueba.com");
-        admin.setContrasena("admin123");
+        admin.setNombre("Admin");
+        admin.setCorreo("admin@gmail.com");
+        admin.setContrasena("1234");
         admin.setRol("ADMINISTRADOR");
-
         usuarioRepository.save(admin);
         return "Administrador creado exitosamente";
     }
